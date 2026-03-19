fef5906e4856c9839bd564eb8b925b75338ac5c2
Nikolove3000 <mynamelathanh193@gmail.com>
2026-03-19 08:52:52 +0700

    Update file structure

--- src/main/java/com/example/lesson01_lifecycle/CacheManager.java
+++ src/main/java/com/example/lesson01_lifecycle/CacheManager.java
@@ -1,4 +1,4 @@
-package com.example.service;
+package com.example.lesson01_lifecycle;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
--- src/main/java/com/example/lesson01_lifecycle/Main.java
+++ src/main/java/com/example/lesson01_lifecycle/Main.java
@@ -1,15 +1,18 @@
-package com.example;
+package com.example.lesson01_lifecycle;
 
+import com.example.lesson02_configuration.service.AppDatabase;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.example")
+@ComponentScan("com.example.lesson01_lifecycle")
 public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(Main.class);
 
+        AppDatabase db = context.getBean(AppDatabase.class);
+        db.connect();
         context.close();
     }
 
